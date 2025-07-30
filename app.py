e6fc214d555b3a51c18901e805ba3d47a9b4a2ca
Marrium2212 <118329753+Marrium2212@users.noreply.github.com>
2025-07-30 08:37:58 -0600

    Update app.py

--- app.py
+++ app.py
@@ -7,15 +7,16 @@ st.title("Penguine Specy Prediction ML app")
 st.info("This is end-to-end Machine Learning APP")
 
 
-with st.expender("Data")
-pass
+with st.expender("Data"):
+ pass
 
-with st.expender("Data Visualization")
-pass
-with st.expender("Input data")
+with st.expender("Data Visualization"):
+ pass
+with st.expender("Input data"):
+ pass
 
-with st.expender("Data Preperation")
-pass
+with st.expender("Data Preperation"):
+ pass
 
 with st.sidebar:
  st.header("Input Variables")
